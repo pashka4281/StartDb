19e4ab4c31128cf06625544cbc473e1f634dde7d
admin <paul@paul-System-Product-Name.(none)>
2011-02-22 20:00:28 +0200

    fixes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,9 @@ class HomeController < ApplicationController
   def index
     @curr_page = 'index'
     @persons = Person.all
-    @last_companies = Company.order('created_at DESC').limit(8)    || []
-    @last_people    = Person.order('created_at DESC').limit(8)     || []
-    @last_jobs      = JobCompany.order('created_at DESC').limit(4) || []
-    @last_events    = Event.order('created_at DESC').limit(4)      || []
+    @last_companies = Company.order('created_at DESC').limit(8)  
+    @last_people    = Person.order('created_at DESC').limit(8)     
+    @last_jobs      = JobCompany.order('created_at DESC').limit(4) 
+    @last_events    = Event.order('created_at DESC').limit(4)    
   end
 end
